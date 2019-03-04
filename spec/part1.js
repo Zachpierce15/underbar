26d2d22a7a2b3d27cc9bab05256208bf764ec357
Zachary <zachary.15pierce@gmail.com>
2019-03-04 08:30:02 -0700

    Changed back the original problem

--- spec/part1.js
+++ spec/part1.js
@@ -10,8 +10,7 @@
 
       it('should return whatever value is passed into it', function() {
         var uniqueObject = {};
-        var one = 1
-        expect(_.identity(one)).to.equal(one);
+        expect(_.identity(1)).to.equal(1);
         expect(_.identity('string')).to.equal('string');
         expect(_.identity(false)).to.equal(false);
         expect(_.identity(uniqueObject)).to.equal(uniqueObject);
